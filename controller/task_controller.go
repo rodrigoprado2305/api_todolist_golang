0be16f4aa08cb31ac6f8a5ed23f8afdea5a0f292
ROD-NOTE\rod_c <rod.crack3r@gmail.com>
2025-03-12 22:22:23 -0300

    Versão 03 - 1 método com uso de GO ROUTINE

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -8,13 +8,23 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+
+	"log"
 )
 
 type TaskController struct {
 	DB *sql.DB
 }
 
+func logRequest(c *gin.Context) { // Chama a goroutine para log
+	go func() {
+		log.Printf("Go routine - Requisição recebida: %s %s", c.Request.Method, c.Request.URL.Path)
+	}()
+}
+
 func (tc *TaskController) CreateTask(c *gin.Context) {
+	logRequest(c)
+
 	var task model.Task
 	if err := c.BindJSON(&task); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -31,6 +41,8 @@ func (tc *TaskController) CreateTask(c *gin.Context) {
 }
 
 func (tc *TaskController) GetTasks(c *gin.Context) {
+	logRequest(c)
+
 	tasks, err := repository.GetTasks(tc.DB)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -40,6 +52,8 @@ func (tc *TaskController) GetTasks(c *gin.Context) {
 }
 
 func (tc *TaskController) GetTask(c *gin.Context) {
+	logRequest(c)
+
 	id, _ := strconv.Atoi(c.Param("id"))
 	task, err := repository.GetTask(tc.DB, id)
 	if err != nil {
@@ -50,6 +64,8 @@ func (tc *TaskController) GetTask(c *gin.Context) {
 }
 
 func (tc *TaskController) UpdateTask(c *gin.Context) {
+	logRequest(c)
+
 	id, _ := strconv.Atoi(c.Param("id"))
 	var task model.Task
 	if err := c.BindJSON(&task); err != nil {
@@ -66,6 +82,8 @@ func (tc *TaskController) UpdateTask(c *gin.Context) {
 }
 
 func (tc *TaskController) DeleteTask(c *gin.Context) {
+	logRequest(c)
+
 	id, _ := strconv.Atoi(c.Param("id"))
 	if err := repository.DeleteTask(tc.DB, id); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
